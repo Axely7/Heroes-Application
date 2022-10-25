72ad54289e40ee2acbb42ede922ca5bd3d1237ab
Axely7 <axely7@hotmail.com>
2022-10-25 08:53:57 -0500

    creating navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,10 +3,13 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth/pages/LoginPage";
 import { DCPage } from "../heroes/pages/DCPage";
 import { MarvelPage } from "../heroes/pages/MarvelPage";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
+
       <Routes>
         <Route path="marvel" element={<MarvelPage />} />
         <Route path="dc" element={<DCPage />} />
