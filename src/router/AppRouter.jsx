23775ece4914b9cef98c073078b45bf39eb1371b
Axely7 <axely7@hotmail.com>
2022-10-26 09:15:35 -0500

    applying routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { LoginPage } from "../auth/pages/LoginPage";
-import { DCPage } from "../heroes/pages/DCPage";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
-import { Navbar } from "../ui";
+import { HeroesRoutes } from "../heroes";
+import { LoginPage } from "../auth";
 
 export const AppRouter = () => {
   return (
     <>
-      <Navbar />
-
       <Routes>
-        <Route path="marvel" element={<MarvelPage />} />
-        <Route path="dc" element={<DCPage />} />
         <Route path="login" element={<LoginPage />} />
-        <Route path="/" element={<Navigate to="/marvel" />} />
+        <Route path="/*" element={<HeroesRoutes />} />
       </Routes>
     </>
   );
